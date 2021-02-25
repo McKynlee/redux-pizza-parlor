cc616a41cd08427dcf76958275e52f7e175922b4
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-25 14:35:37 -0600

    GETting pizzas from db to console

--- src/components/App/PizzaList/PizzaList.jsx
+++ src/components/App/PizzaList/PizzaList.jsx
@@ -1,10 +1,18 @@
+import axios from 'axios';
 
 // GET pizza list from server
 function PizzaList() {
+  let pizzaList;
+
+  axios.get('/api/pizza')
+    .then(response => {
+      console.log('GET /api/pizza:', response.data);
+      pizzaList = response.data
+    })
 
   return (
     <div>
-      TEST PizzaList
+      TEST PizzaList {pizzaList}
     </div>
   );
 } // end PizzaList
